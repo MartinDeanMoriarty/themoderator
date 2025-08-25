756ce49c29bba7280f8b8640eade61e0ed041de6
MartinDean <marti22go@yahoo.de>
2025-08-25 22:59:46 +0200

    yes

--- src/main/java/com/nomoneypirate/config/ModConfig.java
+++ src/main/java/com/nomoneypirate/config/ModConfig.java
@@ -1,18 +1,8 @@
 package com.nomoneypirate.config;
 
 public class ModConfig {
-    // Usually no change needed.
-    public String ollamaURI = "http://localhost:11434/api/generate";
-    // How much timeout in seconds
-    public Integer connectionTimeout = 30;
-    public Integer responseTimeout = 30;
-    // The name of the model used to generate
-    public String model = "mistral-nemo";
     // The name of the language file
     public String languageFileName = "themoderator_de.json";
-    // LLM Logging
-    public Boolean llmLogging = true;
-    public String logFilename = "themoderator_llm.log";
     // Is llm allowed to use BAN?
     public Boolean allowBanCommand = true;
     // Server with a whitelist should use this!
@@ -20,4 +10,15 @@ public class ModConfig {
     // Server without a whitelist should use this!
     // But it is possible to use both.
     public Boolean useBanlist = false;
+    // Usually no change needed.
+    public String ollamaURI = "http://localhost:11434/api/generate";
+    // How much timeout in seconds
+    public Integer connectionTimeout = 30;
+    public Integer responseTimeout = 30;
+    // The name of the model used to generate
+    public String model = "mistral-nemo";
+    // LLM Logging
+    public Boolean llmLogging = true;
+    public String logFilename = "themoderator_llm.log";
+
 }
